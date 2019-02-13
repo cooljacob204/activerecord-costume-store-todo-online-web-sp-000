c2975749025b9f84d7b1edee214a610244a685a1
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-13 17:42:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,8 @@ class CreateCostumes < ActiveRecord::Migration
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      
+      t.integer :size
+      t.string :image_url
     end
   end
 end
