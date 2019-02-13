bb5d0192e7b1ff7c45ffc847e715df8fb42068e2
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-13 17:45:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,8 +5,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :employees
+      t.string :theme
+      t.integer :price
       t.bool :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
