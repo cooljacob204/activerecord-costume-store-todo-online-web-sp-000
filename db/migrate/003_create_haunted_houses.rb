df117b77de3bec387047445d18d3779f3223ab46
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-13 17:53:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening_time
+      t.datetime :opening_date
       t.datetime :closing_time
       t.string :description
       t.timestamps
