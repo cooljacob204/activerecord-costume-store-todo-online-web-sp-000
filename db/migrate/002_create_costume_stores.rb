ebf08778a660aacf90a5f0eed4ca347e2e6206bc
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-13 17:45:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,9 +4,12 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
-      t.integer :size
-      t.string :image_url
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.bool :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
